755637a2cc4086a1b14509d238c9c504f3d8e2a8
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-09-03 16:04:36 +0200

    CARD REALM 0.3.0
    Add Cards Collection Overview
    Update App.js
    Update index.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 function App() {
   return (
     <div className="app">
-      <h1 className="title">CARD REALM</h1>
+      <div className="title">CARD REALM</div>
+      <div className="collection">
+        <Tier tier="COMMON" cardsNumber={6} />
+        <Tier tier="UNCOMMON" cardsNumber={5} />
+        <Tier tier="RARE" cardsNumber={4} />
+        <Tier tier="SUPER RARE" cardsNumber={3} />
+      </div>
+    </div>
+  );
+}
+
+function Tier({ tier, cardsNumber }) {
+  return (
+    <div className="tier">
+      <div className="tier-title">{tier}</div>
+      <div className="cards">
+        {Array(cardsNumber).fill(<Card />)}
+      </div>
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <h2 className="card-title">Card Title</h2>
+      <p className="card-description">Card Description</p>
     </div>
   );
 }
