2c0fa1aec750622d348d2d7713ea3b6d35c2a45a
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-09-05 14:49:18 +0200

    CARD REALM 0.6.0
    Add Feature Open Card Packs
    Update App.js
    Update index.css

--- src/components/App.js
+++ src/components/App.js
@@ -3,36 +3,52 @@ import TiersData from '../data/tiersData';
 
 export default function App() {
   const tiers = TiersData.tiers;
+  const [cardQuantities, setCardQuantities] = useState(() => {
+    const initialQuantities = {};
+    tiers.forEach(tier => {
+      tier.cards.forEach(card => {
+        initialQuantities[`${tier.id}-${card.id}`] = 0;
+      });
+    });
+    return initialQuantities;
+  });
+
+  function openPack() {
+    let randomTier = Math.floor(Math.random() * tiers.length);
+    let randomCard = Math.floor(Math.random() * tiers[randomTier].cards.length);
+    let choseCard = `${randomTier}-${randomCard}`;
+
+    setCardQuantities(prev => ({
+      ...prev,
+      [choseCard]: prev[choseCard] + 1
+    }));
+  }
 
   return (
     <div className="app">
       <div className="title">CARD REALM</div>
-      <div className="collection">
-        {tiers.map((tier) => (<Tier key={tier.id} tier={tier} />))}
+      <div className="panel">
+        {tiers.map((tier) => (<Tier key={tier.id} tier={tier} cardQuantities={cardQuantities} />))}
+        <div className="open-packs">
+          <button className="open-packs-button" onClick={openPack}>OPEN PACKS</button>
+        </div>
       </div>
     </div>
   );
 }
 
-function Tier({ tier }) {
+function Tier({ tier, cardQuantities }) {
   return (
     <div className="tier">
       <div className="tier-title">{tier.name}</div>
       <div className="cards">
-        {tier.cards.map((card) => (<Card key={tier.id + "-" + card.id} tier={tier.name} card={card} />))}
+        {tier.cards.map((card) => (<Card key={tier.id + "-" + card.id} card={card} cantity={cardQuantities[`${tier.id}-${card.id}`]} />))}
       </div>
     </div>
   );
 }
 
-function Card({ tier, card }) {
-  const [cantity, setCantity] = useState(0);
-
-  // TODO: Implement the logic to set the cantity of the card
-  if ((tier === "COMMON" || tier === "UNCOMMON") && cantity === 0) {
-    setCantity(1);
-  }
-
+function Card({ card, cantity }) {
   return (
     <>
       {cantity !== 0 ?
@@ -40,7 +56,8 @@ function Card({ tier, card }) {
           <div className="card-discovered-img-div">
             <img className="card-discovered-img" src={`/img/gems/${card.name}.png`} alt={`${card.name}.png`} />
           </div>
-          <h2 className="card-title">{card.name}</h2></div>
+          <h2 className="card-title">{card.name}</h2>
+        </div>
         :
         <div className="card card-not-discovered">
           <img className="card-not-discovered-img" src="/img/unknown.png" alt="Card not discovered" />
