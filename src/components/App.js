82241bf68c42e80c5f810fd5888c8a71f746f753
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-09-05 12:45:39 +0200

    CARD REALM 0.5.0
    Add Cards Images
    Update App.js
    Update index.css
    Update tiersData.json

--- src/components/App.js
+++ src/components/App.js
@@ -19,27 +19,32 @@ function Tier({ tier }) {
     <div className="tier">
       <div className="tier-title">{tier.name}</div>
       <div className="cards">
-        {tier.cards.map((card) => (<Card key={tier.id + "-" + card.id} card={card} />))}
+        {tier.cards.map((card) => (<Card key={tier.id + "-" + card.id} tier={tier.name} card={card} />))}
       </div>
     </div>
   );
 }
 
-function Card({ card }) {
-  // eslint-disable-next-line no-unused-vars
+function Card({ tier, card }) {
   const [cantity, setCantity] = useState(0);
-  // TODO: Remove the comment
-  // console.log(card.name);
-  // if (card.name === "Card C1" && cantity === 0) {
-  //   setCantity(1);
-  // }
+
+  // TODO: Implement the logic to set the cantity of the card
+  if ((tier === "COMMON" || tier === "UNCOMMON") && cantity === 0) {
+    setCantity(1);
+  }
 
   return (
     <>
       {cantity !== 0 ?
-        <div className="card card-discovered"><h2 className="card-title">{card.name}</h2></div>
+        <div className="card card-discovered">
+          <div className="card-discovered-img-div">
+            <img className="card-discovered-img" src={`/img/gems/${card.name}.png`} alt={`${card.name}.png`} />
+          </div>
+          <h2 className="card-title">{card.name}</h2></div>
         :
-        <div className="card card-not-discovered"><img className="card-not-discovered-img" src="/img/unknown.png" alt="Card not discovered" /></div>}
+        <div className="card card-not-discovered">
+          <img className="card-not-discovered-img" src="/img/unknown.png" alt="Card not discovered" />
+        </div>}
     </>
   );
 }
