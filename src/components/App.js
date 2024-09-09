1f2461ffae7ffbf4c3041a87fbebc0fa5e3b9f42
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-09-09 17:26:51 +0200

    CARD REALM 0.7.0
    Add Feature Card Counter
    Update App.js
    Update index.css

--- src/components/App.js
+++ src/components/App.js
@@ -42,26 +42,41 @@ function Tier({ tier, cardQuantities }) {
     <div className="tier">
       <div className="tier-title">{tier.name}</div>
       <div className="cards">
-        {tier.cards.map((card) => (<Card key={tier.id + "-" + card.id} card={card} cantity={cardQuantities[`${tier.id}-${card.id}`]} />))}
+        {tier.cards.map((card) => (<Card key={tier.id + "-" + card.id} card={card} quantity={cardQuantities[`${tier.id}-${card.id}`]} />))}
       </div>
     </div>
   );
 }
 
-function Card({ card, cantity }) {
+function Card({ card, quantity }) {
   return (
     <>
-      {cantity !== 0 ?
-        <div className="card card-discovered">
-          <div className="card-discovered-img-div">
-            <img className="card-discovered-img" src={`/img/gems/${card.name}.png`} alt={`${card.name}.png`} />
+      {quantity !== 0 ?
+        <div>
+          <div className="card card-discovered">
+            <div className="card-discovered-img-div">
+              <img className="card-discovered-img" src={`/img/gems/${card.name}.png`} alt={`${card.name}.png`} />
+            </div>
+            <h2 className="card-title">{card.name}</h2>
           </div>
-          <h2 className="card-title">{card.name}</h2>
+          <CardCounter quantity={quantity} />
         </div>
         :
-        <div className="card card-not-discovered">
-          <img className="card-not-discovered-img" src="/img/unknown.png" alt="Card not discovered" />
-        </div>}
+        <div>
+          <div className="card card-not-discovered">
+            <img className="card-not-discovered-img" src="/img/unknown.png" alt="Card not discovered" />
+          </div>
+          <CardCounter quantity={quantity} />
+        </div>
+      }
     </>
   );
+}
+
+function CardCounter({ quantity }) {
+  return (
+    <div className="card-counter">
+      <div>{quantity}</div>
+    </div>
+  )
 }
